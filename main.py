f92ca4e1ee0c00fa95f6d7dc6910f1de3b65bd1d
JOSHI-PC\joshu <joshua.koester100@gmail.com>
2024-05-07 15:08:33 +0200

    read in csv ground truth data

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("Hello, World!")
+# import libaries 
+import csv
 
-# try1
+# get csv data
+groundtruthdata = []
+with open('data.csv', 'r') as csv_datei:
+    csv_reader = csv.reader(csv_datei)
+    # skip header
+    next(csv_reader)
+    for zeile in csv_reader:
+        groundtruthdata.append(zeile[:2])
